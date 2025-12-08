1a785b583779ccd998ee1a1f6dadb40145c5d5ce
arvidguo <arvidguo@trussan.com>
2025-12-08 17:43:55 +0800

    fix:调整标题

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000
+    port: 3000,
+    host: '0.0.0.0', // 允许外部访问
+    open: true // 自动打开浏览器
   },
   // GitHub Pages部署时的基础路径，根据实际情况修改
   // 如果部署在https://username.github.io/下，使用'/'
