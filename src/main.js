31abc0d76c0a20d0d81eb9d6d8b8bd397c533a3c
arvidguo <arvidguo@trussan.com>
2025-12-09 18:25:49 +0800

    样式调整

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 // 使用CDN引入的全局Vue对象
 const { createApp } = Vue;
 
-// 导入样式和路由
-import './style.css'
+// 导入路由和组件
 import './utils/rem.js'
 import router from './router/index.js'
 import App from './App.vue'
